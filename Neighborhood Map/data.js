44ac3ecb25d5348d80ee17c798c83269de019f9a
Scott Archer <scottarcher07@att.net>
2017-11-30 14:21:35 -0600

    Add files via upload

---
+++ Neighborhood Map/data.js
@@ -0,0 +1,9 @@
+//These are the locations I have worked and lived//
+var locations = [
+{title: 'desplaines', location: {lat: 42.0221834, lng: -87.9299155}},
+{title: 'lisle', location: {lat: 42.0221834, lng: -87.9299155}},
+{title: 'ronald', location: {lat: 41.9655306, lng: -87.8118652}},
+{title: 'glenellyn', location: {lat: 41.8659181, lng: -82.0934054}},
+{title: 'lombard', location: {lat: 41.8733143, lng: -88.0894339}},
+{title: 'vernonhills', location: {lat: 42.2351077, lng: -87.998117}},
+];
