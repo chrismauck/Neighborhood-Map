45d0f40ac16f12946051eb4cd6d29dbf9d0c2ef1
Scott Archer <scottarcher07@att.net>
2017-12-13 14:09:04 -0600

    Add files via upload

--- Neighborhood Map/data.js
+++ Neighborhood Map/data.js
@@ -7,3 +7,15 @@ var locations = [
 {title: 'lombard', location: {lat: 41.8733143, lng: -88.0894339}},
 {title: 'vernonhills', location: {lat: 42.2351077, lng: -87.998117}},
 ];
+
+
+
+
+//<script> async defer
+//src="https://maps.googleapis.com/maps/api/js?libraries=geometry&key=AIzaSyASuEpaReQgwTJ5_Ud7zAxNc8yO-YBfz4Y&v=3&callback=initMap">
+//</script>
+
+//</script>
+//      <script async defer
+//    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD6ZWHh5NID1cxD67I8FH43LLesybdua7c&v=3&callback=initMap">
+//      </script>
