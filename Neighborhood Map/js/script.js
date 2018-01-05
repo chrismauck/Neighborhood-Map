d459e66e6e1c10ea5ee64d1761002c0a8fcd6a42
Chris Mauck <chrismauck@gmail.com>
2018-01-04 20:58:41 -0600

    Responsiveness and cleanup
    Added responsive CSS values for the location list.
    Added buttons to show and hide location list on sizes below iPad
    portrait.
    Included a check to close location list on location click on mobile.
    Renamed methods in script.js
    Updated comments throughout script.js
    Cleaned up HTML

--- Neighborhood Map/js/script.js
+++ Neighborhood Map/js/script.js
@@ -1,8 +1,7 @@
-// a variable for map starting center position
 var centerLocation = {lat: 41.8333925, lng: -88.0121478};
 var map;
 
-// Location array
+// LOCATION ARRAY
 var locations = [
     {
         category: 'Work',
@@ -53,29 +52,32 @@ var locations = [
         }
     }
 ];
-        
+
+// DEFAULT MESSAGE to display in infowindow while waiting for load
 var infowindowDefault = '<div style="width: 100px; height: 60px;"><p style="text-align: center;">Loading your StreetViewPanorama</p></div>';
 
-var openWindow = false;
+// DEFAULT MESSAGE while waiting for weather to load
+var infowindowContent = '<div class="weatherBug">Loading weather...</div>';
 
-var infowindowContent = 'Error';
-
-// setting up viewmodel
-var ViewModel = function() {
+// SET UP THE viewModel
+var viewModel = function() {
     // assign self to this perimeter so we can easily point objects to viewmodel
     var self = this;
 
     // knockout observable variable for the input given by a user
     self.locationInput = ko.observable("");
 
+    self.weatherData = ko.observable("");
+
     // knockout observable array to store location list items
     self.locationList = ko.observableArray([]);
 
-    // we initialize Loc objects and story them in the ko observable array
+    // we initialize locales objects and story them in the ko observable array
     locations.forEach(function(item){
-        self.locationList.push( new Loc(item) );
+        self.locationList.push( new locales(item) );
     });
 
+    // GENERATE infowindow TITLE, WEATHER AND STREETVIEW
     self.populateInfoWindow = function(location){
         var thisContent;
         var streetViewService = new google.maps.StreetViewService();
@@ -84,7 +86,7 @@ var ViewModel = function() {
             if (status == google.maps.StreetViewStatus.OK) {
                 var nearStreetViewLocation = data.location.latLng;
                 var heading = google.maps.geometry.spherical.computeHeading(nearStreetViewLocation, location.marker.position);
-                self.infowindow.setContent('<div class="locationName"><span>' + location.cat() + ':</span> ' + location.name() + '</div>'+infowindowContent+'<div id="pano"></div>');
+                self.infowindow.setContent('<div class="locationName"><span>' + location.cat() + ':</span> ' + location.name() + '</div>'+self.weatherData+'<div id="pano"></div>');
                 var panoramaOptipons = {
                     position: nearStreetViewLocation,
                     pov: {
@@ -100,11 +102,12 @@ var ViewModel = function() {
         streetViewService.getPanoramaByLocation(location.marker.position, radius, getStreetView);
     };
 
-    // google map infowindow object
+    // SET DEFAULT GOOGLE MAP infowindow OBJECT
     self.infowindow = new google.maps.InfoWindow({
         content: infowindowDefault
     });
 
+    // CALL OPENWEATHERMAP API
     self.weather = function(location) {
         var weatherURL = 'http://api.openweathermap.org/data/2.5/weather?lat=' + location.latLng.lat + '&lon=' + location.latLng.lng + '&units=imperial&APPID=24fe1091a58e1b8c1660ac9a7c2965c0';
         // create settings object for ajax call
@@ -112,7 +115,8 @@ var ViewModel = function() {
             url: weatherURL,
             // set infowindow content to say the provided string when error is thrown
             error: function(XMLHttpRequest, textStatus, errorThrown) {
-                infowindowContent = '<div class="weatherBug">OpenWeatherMap Error</div>';
+                alert("Opps");
+                // infowindowContent = '<div class="weatherBug">OpenWeatherMap Error</div>';
             },
             // what ajax function should do when the http request works fine
             success: function(results) {
@@ -125,7 +129,8 @@ var ViewModel = function() {
                     desc: results.weather[0].description
                 };
                 // self.infowindow.setContent(html);
-                infowindowContent = '<div class="weatherBug" id="weather'+location.name()+'">'+context.status+', '+context.temp+'&deg;F</div>';
+                // infowindowContent = '<div class="weatherBug" id="weather'+location.name()+'">'+context.status+', '+context.temp+'&deg;F</div>';
+                self.weatherData='<div class="weatherBug" id="weather'+location.name()+'">'+context.status+', '+context.temp+'&deg;F</div>';
                 // return weather;
             }
         };
@@ -133,7 +138,7 @@ var ViewModel = function() {
         $.ajax(settings);
     }
 
-    // this function is for displaying infowindow on clicked objects
+    // DISPLAY infowindow ON CLICKED OBJECTS/LOCATION LIST
     self.infoWindowMessage = function(location) {
         // reset all markers to red and set the location infowindow active state to false
         for (var j = 0; j < locations.length; j++) {
@@ -141,23 +146,20 @@ var ViewModel = function() {
             self.locationList()[j].activeWindow("false");
         }
 
+        // important for mobile view. this will change boolean value of self.activeList back to false
+        // effectively changes the class on the body to hide locations when one is selected
+        if ( self.activeList() == true ) self.activeList(!self.activeList());
+
         // reset infowindow content to be "loading" screen
         self.infowindow.setContent(infowindowDefault);
 
         // set clicked marker to a blue marker
         location.marker.setIcon("https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png");
 
-        // animate the marker for a single bounce on click (or menu click - same thing)
-        // moved to separate method when hovering self.hintLocation
-            // location.marker.setAnimation(google.maps.Animation.BOUNCE);
-            // setTimeout (function(){
-            //     location.marker.setAnimation(null);
-            // }, 700);
         self.weather(location);
 
         // populate infowindow
         self.populateInfoWindow(location);
-
 
         // update the activeWindow variable on the location to assist in hinting, etc
         location.activeWindow("true");
@@ -166,6 +168,7 @@ var ViewModel = function() {
         self.infowindow.open(map, location.marker);
     };
 
+    // SHOW HINT ON MOUSEOVER
     self.hintLocation = function(location) {
         // if this location does not have an active infowindow
         if (location.activeWindow() === "false") {
@@ -180,20 +183,23 @@ var ViewModel = function() {
         }
     };
 
+    // HIDE HINT ON MOUSEOUT
     self.removeHint = function(location){
         // if this location does not have an active infowindow
         if (location.activeWindow() === "false") {
             // reset the marker to red
             location.marker.setIcon("https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png");
         }
+        //remove animation
         location.marker.setAnimation(null);
     };
 
-    // iterate through locationList ko observable array
+    // ITERATE THROUGH locationList ko OBSERVABLE ARRAY
     self.locationList().forEach(function(location){
         // add event listener for 'click' to each location.marker then we pass location to the return function, then run it right after with location as parameter
         google.maps.event.addListener(location.marker, 'click', (function(){
             return function(){
+                // self.weather(location);
                 self.infoWindowMessage(location);
             }
         })(location));
@@ -210,7 +216,7 @@ var ViewModel = function() {
         })(location));
     });
 
-    // create a computed list out of locationList ko array and locationInput ko
+    // CREATE A "computed list" OUT OF locationList ko ARRAY AND locationInput ko
     self.computedList = ko.computed(function(){
         // array to store the result
         var tempFilteredList = [], sorted = [];
@@ -228,6 +234,8 @@ var ViewModel = function() {
             }
         }
 
+        // sort the filtered list we just created, relying on the orderByProperty method to 
+        // order the objects within the array by property names
         tempFilteredList.sort(orderByProperty('cat', 'name'));
 
         //build new array separating by location category for dynamic navigation list
@@ -238,9 +246,19 @@ var ViewModel = function() {
             sorted[tempFilteredList[j].cat()].push(tempFilteredList[j]);
         }
 
+        // return the final 'sorted' value
         return sorted;
     });
 
+    // SET DEFAULT VALUE FOR self.activeList AFFECTING BODY CLASS
+    self.activeList = ko.observable(false);
+
+    // TOGGLE THE BOOLEAN OF self.activeList [ changes class on body element ]
+    self.toggleClass = function() {
+        self.activeList(!self.activeList());
+    };
+
+    // **** may not be needed. not complete ****
     self.buttonActions = function() {
         var listLength = self.locationList().length;
         for (var i = 0; i < listLength; i++){
@@ -249,8 +267,8 @@ var ViewModel = function() {
     };
 }
 
-// Loc method creates an object to hold all information for each location 
-var Loc = function(data) {
+// locales METHOD CREATES AN OBJECT TO STORE LOCATION INFORMATION 
+var locales = function(data) {
     // assign self to this variable so we can point objects to viewmodel
     var self = this;
 
@@ -260,7 +278,7 @@ var Loc = function(data) {
     // latitude and longitude to set the marker location
     self.latLng = data.location;
     
-    // image for the marker
+    // default image for the marker
     var image = {
         url: 'https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png',
     };
@@ -288,7 +306,7 @@ var Loc = function(data) {
 }
 
 
-// initialize google map
+// INITIALIZE MAP
 function initMap() {
     var myLatLng = centerLocation;
 
@@ -298,19 +316,11 @@ function initMap() {
         mapTypeId: google.maps.MapTypeId.TERRAIN
     });
 
-    ko.applyBindings(new ViewModel());
+    ko.applyBindings(new viewModel());
 }
 
 
-// additional helper methods
-
-function toggleBounce(marker) {
-    if (marker.getAnimation() !== null) {
-        marker.setAnimation(null);
-    } else {
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
+// ADDITIONAL HELPER METTHODS
 
 function orderByProperty(prop) {
     var args = Array.prototype.slice.call(arguments, 1);
